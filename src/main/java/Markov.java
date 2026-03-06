ce548e94ab8077d3d14fc479ae1c7202bdbfb489
William Casey <wcasey@csumb.edu>
2026-03-06 15:17:56 -0800

    Finished (I hope)

--- src/main/java/Markov.java
+++ src/main/java/Markov.java
@@ -8,7 +8,7 @@ import java.util.Random;
 public class Markov {
     private static final String BEGINS_SENTENCE = "__$";
     private String prevWord;
-    private HashMap<String, ArrayList<String>> words;
+    private HashMap<String, ArrayList<String>> words; //instructions say not final, don't change for warning
     private static final String PUNCTUATION_MARKS = ".!?$";
 
     public Markov() {
@@ -52,8 +52,11 @@ public class Markov {
     }
 
     public void addWord(String word) {
-        if (word == null || word.length() == 0) {
+        if (word == null || word.isEmpty()) {
             return;
+        }
+        if(!words.containsKey(word)) {
+            words.put(word, new ArrayList<>());
         }
         //not sure if this is fully necessary, check back
         if (endsWithPunctuation(prevWord)) {
@@ -61,7 +64,9 @@ public class Markov {
             prevWord = word;
             return;
         }
-
+        if(!words.containsKey(prevWord)) {
+            words.put(prevWord, new ArrayList<>());
+        }
         words.get(prevWord).add(word);
         prevWord = word;
     }
@@ -85,12 +90,19 @@ public class Markov {
     }
 
     public void addLine(String lineIn) {
-        if (lineIn == null || lineIn.length() == 0) {
-            System.out.println("Cant add line, empty/nonexistent");
+        if (lineIn == null || lineIn.isEmpty()) {
+//            System.out.println("Cant add line, empty/nonexistent");
             return;
         }
+        //I can split the line wherever there is a space, good tool to remember
+        String[] separatedWords = lineIn.split(" ");
+        for (int i = 0; i < separatedWords.length; i++) {
+            String currentWord = separatedWords[i];
 
-
+            if (!currentWord.isEmpty()) {
+                addWord(currentWord);
+            }
+        }
 
     }
 
@@ -100,11 +112,7 @@ public class Markov {
                 return false;
             }
             char lastLetter = word.charAt(word.length() - 1);
-                if (PUNCTUATION_MARKS.indexOf(lastLetter) >= 0) {
-                    return true;
-                } else {
-                    return false;
-                }
+            return PUNCTUATION_MARKS.indexOf(lastLetter) >= 0;
         } catch (Exception e) {
             System.out.println("endsWithPunctuation catch exception");
             return false;
