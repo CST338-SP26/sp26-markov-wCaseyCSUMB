5f2507cf79bd6ce567cb2038df897026a331ed07
William Casey <wcasey@csumb.edu>
2026-03-05 22:57:03 -0800

    Method outlines + variables

---
+++ src/main/java/Markov.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Markov {
+    private static final String BEGINS_SENTENCE = "__$";
+    private String prevWord;
+    private HashMap<String, ArrayList<String>> words;
+    private static final String PUNCTUATION_MARKS = ".!?$";
+
+    public Markov() {
+
+    }
+
+    public String getSentence() {
+
+    }
+
+    public void addFromFile(String filename) {
+
+    }
+
+    public void addWord(String word) {
+
+    }
+
+    public String randomWord(String input) {
+
+    }
+
+    public String toString() {
+
+    }
+
+    public HashMap<String, ArrayList<String>> getWords() {
+        return words;
+    }
+
+    public void addLine(String input) {
+
+    }
+
+    public static boolean endsWithPunctuation(String input) {
+
+    }
+
+}
